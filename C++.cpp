b92e3142cbd560f70710377eb6876ddde3759cfb
Quin0019 <96914208+Quin0019@users.noreply.github.com>
2021-12-31 23:12:29 +0900

    Create C++.cpp

---
+++ C++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main () {
+    std::cout << "Hello, world!";
+}
+
+
+// &&&&&&&&&&&&&&&&&&&&&&& //
+// &&&&&&&&&&&&&&&&&&&&&&& //
+// &&&&&&&&&&&&&&&&&&&&&&& //
+// &&&&&&&&&&&&&&&&&&&&&&& //
+  
+import std.core;
+
+using std::cout;
+
+int main() {
+    cout << "Hello, world!";
+}
